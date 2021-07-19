c655d2d2ff90c0f5fc5d08bb482e37fbca276d5a
quangcb <79653975+quangcb@users.noreply.github.com>
2021-07-19 08:26:45 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var navclick= document.getElementById('navbtn');
-console.log(navclick)
-let mobileMenu = document.getElementById('mobileMenu');
-navclick.addEventListener('click',function(){
-    mobileMenu.classList.toggle('active');
-})
+// Click Nav
+var openNav = document.querySelector('.navbtn');
+var navList = document.querySelector('.navList');
+
+// Click toggle Nav
+openNav.addEventListener('click', function () {
+    navList.classList.toggle('active');
+});
